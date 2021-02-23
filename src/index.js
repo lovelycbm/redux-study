ca1113b5b19553a59069ccbe72983b737cd2cf8f
lovelycbm <lovelycbm@lovelycbmui-MacBook-Air.local>
2021-02-23 23:53:34 +0900

    redux day1

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const countModifier = (state = 0) => {
+  return state;
+};
+
+const countStore = createStore(countModifier);
+
+console.log(countStore.getState());
+// let count = 0;
+
+// number.innerText = count;
+
+// const updateText = () => {
+//   number.innerText = count;
+// };
+
+// const handlePlus = () => {
+//   count = count + 1;
+//   updateText();
+// };
+
+// const handleMinus = () => {
+//   count = count - 1;
+//   updateText();
+// };
+
+// plus.addEventListener("click", handlePlus);
+// minus.addEventListener("click", handleMinus);
