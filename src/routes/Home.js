56ce4b95bc2bbdf7a43e5aed3d7ad584d7c63a20
lovelycbm <lovelycbm@lovelycbmui-MacBook-Air.local>
2021-02-28 16:58:59 +0900

    react-redux

---
+++ src/routes/Home.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function Home() {
+  const [text, setText] = useState("");
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+    //console.log(text);
+  }
+
+  return (
+    <>
+      <h1>to do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+export default Home;
