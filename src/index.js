efbc379e92b50df58b0d7e6d6585201869efd9be
lovelycbm <lovelycbm@lovelycbmui-MacBook-Air.local>
2021-02-28 15:55:45 +0900

    vanilla-redux done

--- src/index.js
+++ src/index.js
@@ -7,28 +7,69 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+//액션
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
-  console.log(action);
   switch (action.type) {
     case ADD_TODO:
       //상태를 mutate(변형) 하면 안되기 때문에 state는 그대로 가져오고 값은 넣는다?
-      return [...state, { text: action.text, id: action.id }];
+      const newTodoObj = { text: action.text, id: Date.now() };
+      return [newTodoObj, ...state];
     case DELETE_TODO:
-      return [];
+      //state가 현재 todo용 값임...
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
 };
 
 const store = createStore(reducer);
-
 store.subscribe(() => console.log(store.getState()));
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  ul.innerHTML = "";
+  const toDos = store.getState();
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo, id: Date.now() });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
